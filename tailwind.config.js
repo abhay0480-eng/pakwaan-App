8a116fee7e15f98a844af8d0695baf2abea17dc6
Abhay Kumar <abhaykumar@Abhays-MacBook-Air.local>
2024-10-23 19:06:47 +0530

    first commit learning react from namaste react by akshay saini

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+ module.exports = {
+  content: [
+
+    "./index.html", 
+
+    "./components/**/*.{html,js}",
+
+    "./pages/**/*.{html,js}",
+
+    "./App.js"
+
+  ],
+
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
